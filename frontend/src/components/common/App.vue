e92a750f0d6d80319a7f3a92c2a4d3f0456da76a
mordecaimuvandi16 <mordecaimuvandi16@gmail.com>
2025-07-31 14:51:47 +0000

    Changes before Firebase Studio auto-run

---
+++ frontend/src/components/common/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <AppHeader />
+    <AppNavigation />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import AppHeader from './AppHeader.vue';
+import AppNavigation from './AppNavigation.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppNavigation,
+  },
+};
+</script>
+
+<style>
+/* Add your global styles here */
+</style>
